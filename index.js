d61e01411c28e8ba87e9ff8670adfd50bd73c06b
Liam Neville <lineville@github.com>
2022-06-17 20:53:52 -0700

    cli args

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-import { Octokit } from "octokit";
-import "dotenv/config";
-const octokit = new Octokit({ auth: `${process.env.GH_PAT}` });
+require('dotenv').config()
+const commandLineArgs = require("command-line-args");
+const { Octokit } = require("octokit");
+const octokit = new Octokit({ auth: process.env.GH_PAT });
 
-const is_hubber = async (handle) => {
+const is_org_member = async (org, handle) => {
   try {
-    const member_of_github = await octokit.request('GET /orgs/{org}/members/{username}', {
-      org: 'github',
-      username: handle
-    });
-    return member_of_github.status == 204;
+    const member_of_org = await octokit.request(
+      "GET /orgs/{org}/members/{username}",
+      {
+        org,
+        username: handle,
+      }
+    );
+    return member_of_org.status == 204;
   } catch (e) {
     return false;
   }
-}
+};
 
-const fetch_stargazers = async (owner, repo, per_page = 100) => {
+const fetch_stargazers = async (owner, repo, per_page, limit) => {
   let page = 1;
   let gazers = [];
   let payload = [];
@@ -27,23 +31,35 @@ const fetch_stargazers = async (owner, repo, per_page = 100) => {
     });
     gazers.push(...payload.data);
     page++;
-  } while (payload.data.length == per_page);
+  } while (payload.data.length == per_page && gazers.length < limit);
   return gazers;
 };
 
-
 const main = async () => {
-  const gazers = await fetch_stargazers("github", "gh-valet");
+  const optionDefinitions = [
+    { name: "org", type: String, multiple: false },
+    { name: "repo", type: String, multiple: false},
+    { name: "limit", type: Number, multiple: false },
+  ];
+  const options = commandLineArgs(optionDefinitions);
+
+  const gazers = await fetch_stargazers(options.org, options.repo, 100, options.limit);
   console.log(`Total stars: ${gazers.length}`);
-  const hubbers = await Promise.all(gazers.map(async (gazer) => {
-    const works_at_github = await is_hubber(gazer.login);
-    return {
-      login: gazer.login,
-      is_hubber: works_at_github,
-    }
-  }));
-  console.log(`Hubbers: ${hubbers.filter(h => h.is_hubber).length}`);
-  console.log(`Percentage of hubbers: ${(hubbers.filter(h => h.is_hubber).length / hubbers.length) * 100}%`);
+  const members_of_org = await Promise.all(
+    gazers.map(async (gazer) => {
+      const works_at_org = await is_org_member(options.org, gazer.login);
+      return {
+        login: gazer.login,
+        is_org_member: works_at_org,
+      };
+    })
+  );
+  console.log(`Members: ${members_of_org.filter((h) => h.is_org_member).length}`);
+  console.log(
+    `Percentage of org members: ${
+      (members_of_org.filter((h) => h.is_org_member).length / members_of_org.length) * 100
+    }%`
+  );
 };
 
 main();
